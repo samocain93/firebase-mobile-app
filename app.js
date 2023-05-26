5e14b95436ef4176840b1f8f06a3599bf13f1735
Sam O'Cain <samocain93@gmail.com>
2023-05-26 17:24:38 -0500

    getting set up

---
+++ app.js
@@ -0,0 +1,6 @@
+const addBtn = document.getElementById('add-button');
+const inputBox = document.getElementById('input-field');
+
+addBtn.addEventListener('click', function() {
+    console.log(inputBox.value)
+})

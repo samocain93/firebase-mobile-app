a35567b031a65d34b925889da14a44aec86e8dc3
Sam O'Cain <samocain93@gmail.com>
2023-05-26 22:18:31 -0500

    list part working

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  push,
+} from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js';
+
+const appSettings = {
+  databaseURL: 'https://playground-e937c-default-rtdb.firebaseio.com/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, 'shopping list');
+
 const addBtn = document.getElementById('add-button');
 const inputBox = document.getElementById('input-field');
+const listEl = document.getElementById('shopping-list')
 
-addBtn.addEventListener('click', function() {
-    console.log(inputBox.value)
-})
+addBtn.addEventListener('click', function () {
+    let inputValue = inputBox.value 
+
+    push(shoppingListInDB, inputValue)
+
+   inputBox.value = "";
+
+   listEl.innerHTML += `<li>${inputValue}</li>`;
+   
+});
+
+
